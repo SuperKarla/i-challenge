905dfeefd54def8633388a28ba540f44f9fd2512
anette stidsing <anettestidsing@gmail.com>
2016-12-28 20:15:07 +0100

    merge

--- IChallenge.App/src/app/app.module.ts
+++ IChallenge.App/src/app/app.module.ts
@@ -2,8 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { MaterialModule } from '@angular/material';
 import { AppComponent } from './app.component';
+
 import { HomeComponent } from './home';
 import { ChallengeService } from './shared';
+
+
+import { MenuComponent } from './menu';
+
 import { routing } from './app.routes';
 
 @NgModule({
@@ -15,7 +20,8 @@ import { routing } from './app.routes';
   declarations: [
     // components
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    MenuComponent
   ],
   providers:[
     ChallengeService
